b4d1971d43071280b3dd3caa8b42f15ff578caeb
nearbyjustine <nearbyjustine@gmail.com>
2025-04-16 19:14:46 +0800

    finished annalyn's infiltration and loglevels exercise

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -1,17 +1,24 @@
 static class LogLine
 {
-    public static string Message(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
+  public static string Message(string logLine)
+  {
+    var indexOfColon = logLine.IndexOf(':') + 1;
+    var message = logLine[indexOfColon..logLine.Length];
+    return message.Trim();
+  }
 
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+  public static string LogLevel(string logLine)
+  {
+    int indexOfLastBracket = logLine.IndexOf(']');
+    int indexOfFirstBracket = 1;
+    var logLevel = logLine[indexOfFirstBracket..indexOfLastBracket];
+    return logLevel.ToLower();
+  }
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+  public static string Reformat(string logLine)
+  {
+    var message = LogLine.Message(logLine);
+    var logLevel = LogLine.LogLevel(logLine);
+    return $"{message} ({logLevel})";
+  }
 }
